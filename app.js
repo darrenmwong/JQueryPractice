d16942ad0f421fa93f40934332e16be7c7bc7a13
darrenmwong <darrenwong06@gmail.com>
2014-08-17 18:31:50 -0700

    Stylesheet on add class

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   //add CLass
   $("#addClass").css('border', '2px solid green')
-    .add("<p> HI </p>");  
+    .add("addClassCSS");  
   alert('ITS WORKING');
 });
 
